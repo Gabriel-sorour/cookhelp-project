90f65a4c5af57403d83d0fcd9e6fc420db449102
Gabriel-sorour <gabrielsorour51@gmail.com>
2025-11-24 15:46:28 +0200

    feat: add naviation links to  AppHeader component and import it in App component
    * import Link in AppHeader

--- cookhelp-project/src/App.jsx
+++ cookhelp-project/src/App.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Chatbot from './pages/Chatbot'
 import About from './pages/About'
+import AppHeader from './componentes/AppHeader'
 
 function App() {
 
   return (
     <Router>
+      <AppHeader />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/chatbot" element={<Chatbot />} />
--- cookhelp-project/src/componentes/AppHeader.jsx
+++ cookhelp-project/src/componentes/AppHeader.jsx
@@ -1,10 +1,16 @@
-function AppHeader(){
+import { Link } from 'react-router-dom'
 
-  return(
-    <nav className="app-header">
-      
-    </nav>
-  )
-}
+  function AppHeader() {
+
+    return (
+      <nav className="app-header">
+        <ul>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/chatbot'>Chatbot</Link></li>
+          <li><Link to='/about'>About</Link></li>
+        </ul>
+      </nav>
+    )
+  }
 
 export default AppHeader;
