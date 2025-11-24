3a08330e043bdc8b82f2421a9622747b30d367ba
Gabriel-sorour <gabrielsorour51@gmail.com>
2025-11-24 21:32:42 +0200

    feat: add and style the About page

--- cookhelp-project/src/pages/About.jsx
+++ cookhelp-project/src/pages/About.jsx
@@ -1,7 +1,26 @@
+import '../styles/About.css'
+
 function About() {
-  
-  return(
-    <p>About page</p>
+
+  return (
+    <main>
+      <section className="about">
+        <h2>About This Project</h2>
+
+        <h3>Purpose</h3>
+        <p>This project is an exercise to practice what I have learned in React so far. It is part of my <strong>portfolio</strong>.</p>
+
+        <h3>Reason for Building</h3>
+        <p>The goal was to simulate a website that makes cooking easier and more enjoyable.</p>
+
+        <h3>Technologies Used</h3>
+        <p>React, Vite, CSS, JavaScript (ES6), React Router.</p>
+
+        <h3>Contributors</h3>
+        <p>Gabriel Sorour.</p>
+      </section>
+    </main>
+
   )
 }
 
