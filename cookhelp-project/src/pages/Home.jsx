8f206e809d746ab9c25c13c93b8bf17c83fb211b
Gabriel-sorour <gabrielsorour51@gmail.com>
2025-11-24 18:00:14 +0200

    feat: add action to Start now button * add  goToChatbot fucntion to the start now button to move to the chabot page using useNavigate hook

--- cookhelp-project/src/pages/Home.jsx
+++ cookhelp-project/src/pages/Home.jsx
@@ -1,13 +1,20 @@
+import { useNavigate } from 'react-router-dom';
 import HomeBanner from '../assets/home-banner.png'
 import '../styles/Home.css'
 
 function Home() {
+  const navigate = useNavigate();
+  function goToChatbot() {
+    navigate('/chatbot');
+  }
 
   return (
     <main>
       <section className='banner-container'>
         <img src={HomeBanner} alt="home-banner" />
-        <button>
+        <button
+          onClick={goToChatbot}
+        >
           Start now !
         </button>
       </section>
