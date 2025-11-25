2db3c371ee10dd4163b2fab2cea157f3b00c6f16
Gabriel-sorour <gabrielsorour51@gmail.com>
2025-11-25 11:28:04 +0200

    feat: create ChatbotInput component * add messages state

--- src/pages/Chatbot.jsx
+++ src/pages/Chatbot.jsx
@@ -1,11 +1,38 @@
+import { useState } from 'react';
 import '../styles/Chatbot.css'
 
+function ChatbotInput() {
+
+  return (
+    <div className='chatbot-input'>
+      <input placeholder='Send message to Chatbot' />
+      <button>
+        Send
+      </button>
+    </div>
+  )
+}
+
+
+
+
 function Chatbot() {
-  
-  return(
+  const [messages, setMessages] = useState([{
+    sender: 'user',
+    text: 'Hello Chatbot!',
+    id: crypto.randomUUID()
+  },
+  {
+    sender: 'robot',
+    text: 'Hello User!',
+    id: crypto.randomUUID()
+  }
+  ]);
+
+  return (
     <main className="chatbot-main">
       <section className='chatbot-container'>
-      
+        <ChatbotInput />
       </section>
     </main>
   )
