e2b108a6dc39215426f7fdf9c27bc6c4cb6e68f8
Gabriel-sorour <gabrielsorour51@gmail.com>
2025-11-26 10:51:53 +0200

    style: add highlight effect to the current page

--- src/componentes/AppHeader.jsx
+++ src/componentes/AppHeader.jsx
@@ -1,17 +1,36 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import '../styles/AppHeader.css'
 
-  function AppHeader() {
+function AppHeader() {
 
-    return (
-      <nav className="app-header">
-        <ul>
-          <Link to='/'><li>Home</li></Link>
-         <Link to='/chatbot'> <li>Chatbot</li></Link>
-          <Link to='/about'><li>About</li></Link>
-        </ul>
-      </nav>
-    )
-  }
+  const location = useLocation();
+
+  return (
+    <nav className="app-header">
+      <ul>
+        <Link
+          className={location.pathname === '/' ? 'link-hover' : ''}
+          to='/'
+        >
+          <li>Home</li>
+        </Link>
+
+        <Link
+          className={location.pathname === '/chatbot' ? 'link-hover' : ''}
+          to='/chatbot'
+        >
+          <li>Chatbot</li>
+        </Link>
+
+        <Link
+          className={location.pathname === '/about' ? 'link-hover' : ''}
+          to='/about'
+        >
+          <li>About</li>
+        </Link>
+      </ul>
+    </nav>
+  )
+}
 
 export default AppHeader;
