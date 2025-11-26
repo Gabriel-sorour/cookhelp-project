960aa287f2ef454c221a6c95c53835b94dbff115
Gabriel-sorour <gabrielsorour51@gmail.com>
2025-11-26 12:17:04 +0200

    fix: allow ngrok host in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/cookhelp-project/',
+   server: {
+    allowedHosts: ['austere-craniometrically-florencia.ngrok-free.dev']
+  }
 })
